64c1b2cd4bd28f7c9c5cec814e751e2538e6705e
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2022-09-10 07:24:53 +0430

    Initial Commit

---
+++ server/src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
+import * as cookieParser from 'cookie-parser';
+
+import { AppModule } from './app.module';
+
+async function bootstrap()
+{
+    const logger: Logger = new Logger('Bootstrap');
+    const app = await NestFactory.create(AppModule, { cors: true });
+    const port: number = +process.env.PORT || 4001;
+
+    app.useGlobalPipes(new ValidationPipe());
+    app.use(cookieParser());
+
+    await app.listen(port);
+    logger.log(`Application listening on port ${port}`);
+}
+
+bootstrap();
